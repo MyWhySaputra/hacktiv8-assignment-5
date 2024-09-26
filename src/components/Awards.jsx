3d704fd881ff91617209693a9c65648a7e74fad2
MyWahyuSap <wahyusaputra222000@gmail.com>
2024-09-26 23:15:53 +0700

    feat: implementation react router dom

---
+++ src/components/Awards.jsx
@@ -0,0 +1,11 @@
+export default function Awards() {
+  return (
+    <div>
+      <h2 className="text-3xl font-bold mb-4">Awards</h2>
+      <ul className="list-disc ml-6">
+        <li>Best Developer Award 2023</li>
+        <li>Hackathon Winner 2022</li>
+      </ul>
+    </div>
+  );
+}
